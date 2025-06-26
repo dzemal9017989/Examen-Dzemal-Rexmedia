500fe9bf1e7f565affe78776a8b48b7e04d5240e
dzemal9017989 <9017989@student.zadkine.nl>
2025-06-26 17:29:05 +0200

    Examen Suriname (26-6-2025)

---
+++ Rexmedia examen/full stack/frontend/src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/Login.vue'
+import TakenView from '@/views/TakenView.vue'
+import Toevoegen from '../views/Toevoegen.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView // <== deze toevoegen
+    },
+    {
+      path: '/taken',
+      name: 'taken',
+      component: TakenView // <== deze toevoegen
+    },
+    {
+      path: '/toevoegen',
+      name: 'toevoegen',
+      component: Toevoegen // <== deze toevoegen
+    }
+  ]
+})
+
+export default router
