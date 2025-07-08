05bf5fbb06b2ed9b79386ec059f235691baea061
dzemal9017989 <9017989@student.zadkine.nl>
2025-07-08 12:22:28 +0200

    Enhance comments for clarity in migration updating 'status_id' on tasks table

--- backend/database/migrations/2025_07_08_082123_update_status_id_on_tasks_table.php
+++ backend/database/migrations/2025_07_08_082123_update_status_id_on_tasks_table.php
@@ -11,15 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // This updates the tasks table
         Schema::table('tasks', function (Blueprint $table) {
+            // This makes the 'status_id' optional (nullable) and it changes the column 
             $table->unsignedBigInteger('status_id')->nullable()->change();
+            // This adds a foreign key t 'status_id' that points to the 'id' of the statuses table 
             $table->foreign('status_id')->references('id')->on('statuses')->onDelete('cascade');
         });
     }
 
     public function down(): void
     {
+        // This updates the table 'tasks' again
         Schema::table('tasks', function (Blueprint $table) {
+            // This removes the external key binding on 'status_id'
             $table->dropForeign(['status_id']);
         });
     }
